10669fa665cacda0c9e4de5b56e28450e8ecdfa7
Billy Trabaudo <btrabaudo@gmail.com>
2017-04-11 16:21:06 -0600

    Created basic html

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>HTML Sandbox 2</title>
+    </head>
+    <body>
+    <h1>Hi! Welcome to my HTML Page</h1>
+    </body>
+</html>
