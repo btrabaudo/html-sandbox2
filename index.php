2fe1733b994662cc771b342dfbe7a37626175bcd
Billy Trabaudo <btrabaudo@gmail.com>
2017-04-12 08:21:41 -0600

    Created basic html

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
     <head>
         <meta charset="utf-8">
         <title>HTML Sandbox 2</title>
+
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="css/style.css" type="text/css"
+
     </head>
     <body>
     <h1>Hi! Welcome to my HTML Page</h1>
