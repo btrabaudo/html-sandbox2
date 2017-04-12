865b366ceace4802468cbe5c17293a78d042d4f5
Billy Trabaudo <btrabaudo@gmail.com>
2017-04-12 09:03:49 -0600

    added JS file

--- index.php
+++ index.php
@@ -6,10 +6,22 @@
 
         <!-- Custom CSS -->
         <link rel="stylesheet" href="css/style.css" type="text/css">
+
         <!--javascript -->
         <script type="text/javascript" src="js/scripts.js"></script>
     </head>
     <body>
-    <h1>Hi! Welcome to my HTML Page</h1>
+        <header>
+            <h1>HTML SANDBOX</h1>
+        </header>
+
+        <main>
+            <h2>Silent Bob Says</h2>
+            <p>Banana</p>
+            <img src="img/banana.jpg" alt="banana"/>
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+            <p>This is Robbie's favorite webstie: <a href="http://www.espn.com" target="_blank">ESPN</a></p>
+            <p>This is a link to an internal page <a href="about/index.php">About Page</a></p>
+        </main>
     </body>
 </html>
