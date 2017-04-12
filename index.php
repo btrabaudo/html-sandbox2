df1928142e4903ae70458c774d2e3d652c604541
Billy Trabaudo <btrabaudo@gmail.com>
2017-04-12 09:37:33 -0600

    added JS file

--- index.php
+++ index.php
@@ -22,6 +22,43 @@
             <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
             <p>This is Robbie's favorite webstie: <a href="http://www.espn.com" target="_blank">ESPN</a></p>
             <p>This is a link to an internal page <a href="about/index.php">About Page</a></p>
+
+            <h2>Some Stuff</h2>
+                <ul>
+                    <li>Beer</li>
+                    <li>More Beer</li>
+                    <li>Other Stuff</li>
+                </ul>
+
+            <h2>Some More Stuff</h2>
+                <ol>
+                    <li>Trolls</li>
+                    <li>Bridges</li>
+                </ol>
+
+            <h2>Cats &amp; Dogs</h2>
+                <table>
+                    <tr>
+                        <th>Cats</th>
+                        <th>Dogs</th>
+                    </tr>
+                    <tr>
+                        <td>tabby</td>
+                        <td>bulldog</td>
+                    </tr>
+                    <tr>
+                        <td>persian</td>
+                        <td>begale</td>
+                    </tr>
+                    <tr>
+                        <td>Senator</td>
+                        <td>CEO</td>
+                    </tr>
+                    <tr>
+                        <td>grumpy</td>
+                        <td>happy</td>
+                    </tr>
+                </table>
         </main>
     </body>
 </html>
